7f951ece2de6ce23cbf08bf37912c76e0556fa0d
sakuxz <yui12327@gmail.com>
2017-01-25 18:12:37 +0000

    add webduino module

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var webduino = require('webduino-js');
 
 let ledState = {
   open: false,
 }
+
+var rgbled;
+
+board = new webduino.WebArduino('10XMWGQM');
+board.on('ready', function () {
+  board.systemReset();
+  board.samplingInterval = 50;
+  rgbled = getRGBLedCathode(board, 15, 12, 13);
+});
 
 router.get('/status', function(req, res, next) {
   res.json(ledState);
@@ -11,6 +21,11 @@ router.get('/status', function(req, res, next) {
 
 router.post('/led', function(req, res, next) {
   ledState.open = (req.body.open == 'true') ? true : false;
+  if (ledState.open) {
+    rgbled.setColor('#00cccc');
+  } else {
+    rgbled.setColor('#000000');
+  }
   res.json({
     status: true,
     data: {
